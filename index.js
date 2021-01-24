bf09c26e50fd24665d32a604865bcf62623e4d62
Ichthyological Dev <ichthyologicaldev@gmail.com>
2021-01-24 14:41:33 -0800

    add and roundDownToTens

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ You will add your code on the line or lines that start with //TODO:
 
 EXAMPLE:
 //REQUIREMENT 1: Do Ichthyologists study fish, birds, or mammals?
-//TODO: On the following line, change null to your answer. Make sure that it's a string!
+//TODO: On the following lines, change null to your answer. Make sure that it's a string!
 let ichthyologistFocus = null;
 
 You would change the previous line so that it says: 
@@ -26,61 +26,44 @@ video: https://youtu.be/vkWDQzl_x3s before continuing.
 I hope that you get hooked on coding!
 */
 
-//REQUIREMENT 1: Do Ichthyologists study fish, birds, or mammals?
-//TODO: On the following line, change null to your answer. Make sure that it's a string!
-let ichthyologistFocus = null;
-
-//REQUIREMENT 2: What is your name?
-//TODO: On the following line, change null to your answer. Make sure that it's a string!
-let developerName = null;
-
-//REQUIREMENT 3: How many oceans are there?
-//TODO: On the following line, change null to your answer. Make sure that it's a number!
-let numberOfOceans = null;
-
-//REQUIREMENT 4: True or false, you are having fun wielding your knowledge of JavaScript data types!
-//TODO: On the following line, change null to your answer. Make sure that it's a boolean!
-let havingFun = null;
-
-//REQUIREMENT 5: What is the state fish of Hawaii? (The one that's fun to say!)
-//TODO: On the following line, change null to your answer. Make sure that it's a string!
-let hawaiianStateFish = null;
-
-//REQUIREMENT 6: How many kilometers long is the Congo River?
-//TODO: On the following line, change null to your answer. Make sure that it's a number!
-let congoLength = null;
-
-//REQUIREMENT 7: True or false, you have caught a fish.
-//TODO: On the following line, change null to your answer. Make sure that it's a boolean!
-let haveCaughtFish = null;
-
-//REQUIREMENT 8: The largest regular number that JavaScript can reliably handle is 9007199254740991. Make a BigInt that's larger!
-//TODO: On the following line, change null to your answer. Make sure that it's a BigInt!
-let bigInteger = null;
-
-//REQUIREMENT 9: True or false, JavaScript is rarely used in large tech companies.
-//TODO: On the following line, change null to your answer. Make sure that it's a boolean!
-let javaScriptIsRarelyUsed = null;
-
-//REQUIREMENT 10: True or false, you can use the BigInt data type with decimals.
-//TODO: On the following line, change null to your answer. Make sure that it's a boolean!
-let bigIntWorksForDecimals = null;
-
-//REQUIREMENT 11: How would you store the answer to (9007199254740992 + 1) as a BigInt?
-//TODO: On the following line, change null to your answer. Make sure that it's a BigInt!
-let largeNumber = null;
-
-//REQUIREMENT 12: How many species of salmon are native to North America?
-//TODO: On the following line, change null to your answer. Make sure that it's a number!
-let totalNaSalmonSpecies = null;
-
-//REQUIREMENT 13: What is the longest river in the world?
-//TODO: On the following line, change null to your answer. Make sure that it's a string!
-let longestRiver = null;
-
-//REQUIREMENT 14: True or false, the previous requirements covered all possible JavaScript data types.
-//TODO: On the following line, change null to your answer. Make sure that it's a boolean!
-let seenAllTypes = null;
+/*
+REQUIREMENT My calculator is broken. When I try to do 5 + 5, it returns 55.
+*/
+//TODO: On the following lines, create a function named add that takes in two parameters that are either strings or numbers and returns the numeric sum
+const add = (a,b) => +a + +b
+/*
+REQUIREMENT In my fishing guide service, I use a unique way of calculating tips. I round every guide's tips down to the nearest 10, and give the excess to the boat cleaner.
+*/
+//TODO: On the following lines, create a function named roundDownToTens that takes in a positive integer and returns that integer rounded down to the nearest 10
+const roundDownToTens = a => a - a%10
+/*
+REQUIREMENT I took out a couple interest only loans to launch this guide service. I think that I'm being overcharged. Help me out! If I just pay the interest amount, what should my monthly payments be? Write a function that takes in the principal and interest rate, and returns the monthly interest-only payment.
+*/
+//TODO: On the following lines, create a function named '' that takes in <> and returns <>
+/*
+REQUIREMENT There's a lot of taxes involved in business. How much am I really paying? Write a function that takes in a single number, and returns the actual tax rate based on the following tax brackets:
+*/
+//TODO: On the following lines, create a function named '' that takes in <> and returns <>
+/*
+REQUIREMENT I like helping out other small business owners. I gave someone $1000 to help launch their pizza company. We haven't worked out the interest rate yet, but they guaranteed that they will pay it back in full 1 year later. In addition to the original $1000, how much interest will this person me? 
+*/
+//TODO: On the following lines, create a function named '' that takes in <> and returns <>
+/*
+REQUIREMENT I hand out coupons for 50% off the next fishing trip to anyone who fails to catch a fish. Prices change every year, so write me a function that calculates the post-coupon cost given an initial price.
+*/
+//TODO: On the following lines, create a function named '' that takes in <> and returns <>
+/*
+REQUIREMENT I have 1 bait bucket per client. I want an equal amount of bait in each bucket at the start of the day. I want to know how much pieces of bait will be left over after placing an equal amount in each bucket. Write a function that takes in a number of buckets and number of pieces of bait, and returns the remainder.
+*/
+//TODO: On the following lines, create a function named '' that takes in <> and returns <>
+/*
+REQUIREMENT I get calls when the fishing heats up in certain areas. I only move to that spot if I can get there in less than 30 minutes. Given a top speed in miles per hour, and a distance in miles, return the number of minutes it will take to reach the destination.
+*/
+//TODO: On the following lines, create a function named '' that takes in <> and returns <>
+/*
+REQUIREMENT The pizza restaurant owner just informed me that they might need to wait longer before paying me back. How much will they owe me? 
+*/
+//TODO: On the following lines, create a function named '' that takes in <> and returns <>
 
 /*
 You've reached the end of this exercise! If you need more practice with data types, you're in luck.
